74995c926e9ed0a37c71f403c3c423e34ec893df
umar-anwar-sdk <146811262+umar-anwar-sdk@users.noreply.github.com>
2023-10-26 13:52:17 +0500

    Countdown Timer

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var countDownDate = new Date("Dec 24, 2023 00:00:00").getTime();
+var x = setInterval(function() {
+    var now = new Date().getTime();
+    var distance = countDownDate - now;
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("secounds").innerHTML = seconds;
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("secounds").innerHTML = "00";
+
+    }
+
+}, 1000);
